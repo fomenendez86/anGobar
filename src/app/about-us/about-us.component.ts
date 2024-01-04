86b860a5656af236fdc770656f003916d67df86b
Franko Menendez <frankomzg@gmail.com>
2024-01-04 10:19:14 -0500

    shorted text for statics pages

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
 
+  subSuccessMessage = '';
+  subErrorMessage = '';
+
+  applyForm = new FormGroup({
+    fullName: new FormControl(''),
+    email: new FormControl('')
+  });
+
+  subscribeEmail() {}
 }
